1d3edb397f8fba57760072416b2acd07e58e62d4
MrBlackLife <amirali@hackermail.com>
2017-06-01 11:18:32 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -232,7 +232,7 @@ function create_config( )
 	"botmanager",
     "tabchi"
     },
-    sudo_users = {122099838},--Sudo users
+    sudo_users = {122099838,305941305},--Sudo users
   }
   serialize_to_file(config, './data/config.lua')
   print('saved config into ./data/config.lua')
